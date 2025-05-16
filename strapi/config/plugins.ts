c0a99b2e4402c4e2001de31f966ac4cc1f40333e
Adam Wheeler <ajwtech@gmail.com>
2025-05-16 08:45:00 -0400

    fix: Allow azure storage through the CSP, Add plugin config for azure storage

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-azure-storage',
+      providerOptions: {
+        authType: 'default',
+        account: process.env.STORAGE_ACCOUNT_NAME,
+        accountKey: process.env.STORAGE_ACCOUNT_KEY,
+        containerName: 'assets',
+        defaultPath: 'assets',
+      },
+    },
+  },
+});

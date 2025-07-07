67023bc7e2f11ef3687379773186c7d35c8dd086
Adam Wheeler <ajwtech@gmail.com>
2025-07-07 04:38:24 -0400

    fix: update promoteEE flag to disable feature by default

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -12,7 +12,7 @@ export default ({ env }) => ({
   },
   flags: {
     nps: env.bool("FLAG_NPS", true),
-    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", false),
   },
   preview: {
     enabled: true,
